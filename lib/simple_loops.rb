6ac55e3de12eab94956dab818dc89b4b46dc562a
Sarah Barton <sarahhtbarton@gmail.com>
2021-02-11 00:33:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,24 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  
+end
+puts loop_message_five_times('wazzup')
+
+
+def loop_messge_n_times(string, integer)
+  
+end
+puts loop_messge_n_times('wazzup', 7)
+
+
+def output_array()
+  
+end
+puts output_array()
+
+
+def return_string_array()
+  
+end
+puts return_string_array()
