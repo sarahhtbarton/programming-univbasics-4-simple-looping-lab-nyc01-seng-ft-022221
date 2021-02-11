232b914a72c7c3f4dc4abe4e278f2f155b95b59d
Sarah Barton <sarahhtbarton@gmail.com>
2021-02-11 00:45:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -31,9 +31,10 @@ puts output_array(["All great achievements", "require time", "- Maya Angelou"])
 
 
 def return_string_array(array)
-  count = 0 
+  count = 0
+  new_array = []
   while count < array.length do
-    array[count].to_s
+    new_array.push(array[count].to_s)
     count += 1 
   end
 end
